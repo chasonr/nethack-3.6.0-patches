d2a78ecd051545e26975ff5ad103f63e93ea0304
Ray Chason <Ray Chason>
2016-01-01 15:13:33 -0500

    Make this functional with win32

--- win/sdl2/sdl2font_windows.cpp
+++ win/sdl2/sdl2font_windows.cpp
@@ -6,6 +6,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <wchar.h>
+extern "C" {
+#include "hack.h"
+}
+#include "unicode.h"
 #include "sdl2font.h"
 
 struct FontImpl
@@ -13,6 +17,61 @@ struct FontImpl
     HFONT hfont;
     HDC memory_dc;
 };
+
+namespace {
+
+wchar_t *
+singleChar(utf32_t ch, wchar_t str16[3])
+{
+    if (ch > 0x10FFFF || (ch & 0xFFFFF800) == 0xD800) {
+        ch = 0xFFFD;
+    }
+
+    if (ch < 0x10000) {
+        str16 = (wchar_t *) str_mem_alloc(2 * sizeof(wchar_t));
+        str16[0] = (wchar_t) ch;
+        str16[1] = 0;
+    } else {
+        str16 = (wchar_t *) str_mem_alloc(3 * sizeof(wchar_t));
+        str16[0] = 0xD7C0 + (ch >> 10);
+        str16[1] = 0xDC00 + (ch & 0x3FF);
+        str16[2] = 0;
+    }
+    return str16;
+}
+
+
+/* UTF-8 to UTF-16 conversion; but don't depend on wchar_t being the same type
+   as utf16_t */
+wchar_t *
+win32String(const char *str)
+{
+    str_context ctx = str_open_context("win32String");
+
+    utf16_t *str16;
+    wchar_t *wstr16;
+    size_t size16, i;
+
+    str16 = uni_8to16(str);
+    size16 = uni_length16(str16);
+    wstr16 = (wchar_t *) str_mem_alloc((size16 + 1) * sizeof(wstr16[0]));
+    for (i = 0; str16[i] != 0; ++i) {
+        wstr16[i] = str16[i];
+    }
+    wstr16[i] = 0;
+    str_export(ctx, wstr16);
+    str_close_context(ctx);
+    return wstr16;
+}
+
+}
+
+static SDL_Surface *renderImpl(
+        void *m_impl,
+        const wchar_t *text,
+        SDL_Color foreground,
+        SDL_Color background);
+static SDL_Rect textSizeImpl(void *m_impl, const wchar_t *text);
 
 SDL2Font::SDL2Font(const char *name, int ptsize) : m_impl(NULL)
 {
@@ -105,33 +164,42 @@ int SDL2Font::fontHeight(void)
     return metrics.tmHeight;
 }
 
+static const SDL_Color transparent = { 0, 0, 0, 0 };
+
 // Text rendering
 // If no background is given, background is transparent
 SDL_Surface *SDL2Font::render(utf32_t ch, SDL_Color foreground)
 {
-    return render(ch, foreground, (SDL_Color){ 0, 0, 0, 0 });
+    return render(ch, foreground, transparent);
 }
 
-SDL_Surface *SDL2Font::render(const std::string& text, SDL_Color foreground)
+SDL_Surface *SDL2Font::render(const char *text, SDL_Color foreground)
 {
-    return render(text, foreground, (SDL_Color){ 0, 0, 0, 0 });
+    return render(text, foreground, transparent);
 }
 
 SDL_Surface *SDL2Font::render(utf32_t ch, SDL_Color foreground, SDL_Color background)
 {
-    wchar_t utf16[2];
-    std::size_t size;
+    wchar_t wch[3];
 
-    size = unicode::put_next_cp(utf16, ch);
-    return render(std::wstring(utf16, size), foreground, background);
+    return renderImpl(m_impl, singleChar(ch, wch), foreground, background);
 }
 
-SDL_Surface *SDL2Font::render(const std::string& text, SDL_Color foreground, SDL_Color background)
+SDL_Surface *SDL2Font::render(const char *text, SDL_Color foreground, SDL_Color background)
 {
-    return render(unicode::convert<wchar_t>(text), foreground, background);
+    str_context ctx = str_open_context("SDL2Font::render");
+    SDL_Surface *surface;
+
+    surface = renderImpl(m_impl, win32String(text), foreground, background);
+    str_close_context(ctx);
+    return surface;
 }
 
-SDL_Surface *SDL2Font::render(const std::wstring& text, SDL_Color foreground, SDL_Color background)
+static SDL_Surface *renderImpl(
+        void *m_impl,
+        const wchar_t *text,
+        SDL_Color foreground,
+        SDL_Color background)
 {
     FontImpl *font = reinterpret_cast<FontImpl *>(m_impl);
     HFONT hfont = font->hfont;
@@ -157,7 +225,7 @@ SDL_Surface *SDL2Font::render(const std::wstring& text, SDL_Color foreground, SD
     if (!SetBkMode(memory_dc, OPAQUE)) goto error;
 
     // Get the size of the bitmap
-    if (!GetTextExtentPoint32W(memory_dc, text.c_str(), text.size(), &size)) goto error;
+    if (!GetTextExtentPoint32W(memory_dc, text, wcslen(text), &size)) goto error;
     // Bitmap creation fails if the size is zero
     if (size.cx == 0) { size.cx = 1; }
     if (size.cy == 0) { size.cy = 1; }
@@ -175,7 +243,7 @@ SDL_Surface *SDL2Font::render(const std::wstring& text, SDL_Color foreground, SD
     if (!SelectObject(memory_dc, (HGDIOBJ)hbitmap)) goto error;
 
     // Render the text
-    if (!TextOutW(memory_dc, 0, 0, text.c_str(), text.size())) goto error;
+    if (!TextOutW(memory_dc, 0, 0, text, wcslen(text))) goto error;
 
     // Get the bitmap
     memset(&bi, 0, sizeof(bi));
@@ -254,25 +322,28 @@ error:
 // Text extent
 SDL_Rect SDL2Font::textSize(utf32_t ch)
 {
-    wchar_t utf16[2];
-    std::size_t size;
+    wchar_t utf16[3];
 
-    size = unicode::put_next_cp(utf16, ch);
-    return textSize(std::wstring(utf16, size));
+    return textSizeImpl(m_impl, singleChar(ch, utf16));
 }
 
-SDL_Rect SDL2Font::textSize(const std::string& text)
+SDL_Rect SDL2Font::textSize(const char *text)
 {
-    return textSize(unicode::convert<wchar_t>(text));
+    str_context ctx = str_open_context("SDL2Font::render");
+    SDL_Rect rect;
+
+    rect = textSizeImpl(m_impl, win32String(text));
+    str_close_context(ctx);
+    return rect;
 }
 
-SDL_Rect SDL2Font::textSize(const std::wstring& text)
+static SDL_Rect textSizeImpl(void *m_impl, const wchar_t *text)
 {
     FontImpl *font = reinterpret_cast<FontImpl *>(m_impl);
     SIZE size;
 
     // Get the size of the text
-    GetTextExtentPoint32W(font->memory_dc, text.c_str(), text.size(), &size);
+    GetTextExtentPoint32W(font->memory_dc, text, wcslen(text), &size);
 
     SDL_Rect rect;
     rect.x = 0;
