3ee278a1f04683b68bd2ab0a974d1d65eb5a5207
Ray Chason <chasonr@gmail.com>
2016-02-28 20:40:39 -0500

    Use BMP for the packed file format.
    The tile set format is now a standard BMP when a VESA mode is selected.
    The tile set is still limited to 16x16 tiles and 256 colors.

---
+++ include/tileset.h
@@ -0,0 +1,39 @@
+#ifndef TILESET_H
+#define TILESET_H
+
+struct Pixel {
+    unsigned char r, g, b, a;
+};
+
+struct TileImage {
+    /* Image data */
+    unsigned width, height;
+    struct Pixel *pixels; /* for direct color */
+    unsigned char *indexes; /* for paletted images */
+};
+
+boolean FDECL(read_tiles, (const char *filename, BOOLEAN_P true_color));
+const struct Pixel *NDECL(get_palette);
+void NDECL(free_tiles);
+const struct TileImage *FDECL(get_tile, (unsigned tile_index));
+
+/* Used internally by the tile set code */
+struct TileSetImage {
+    /* Image data */
+    unsigned width, height;
+    struct Pixel *pixels; /* for direct color */
+    unsigned char *indexes; /* for paletted images */
+    struct Pixel palette[256];
+    
+    /* Image description from the file */
+    char *image_desc;
+
+    /* Tile dimensions */
+    unsigned tile_width, tile_height;
+};
+
+boolean FDECL(read_bmp_tiles, (const char *filename, struct TileSetImage *image));
+boolean FDECL(read_gif_tiles, (const char *filename, struct TileSetImage *image));
+boolean FDECL(read_png_tiles, (const char *filename, struct TileSetImage *image));
+
+#endif
