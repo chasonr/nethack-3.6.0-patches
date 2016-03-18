e4ef1ab15191a865c1f88730620eb6edca58abff
Ray Chason <Ray Chason>
2016-03-18 02:26:52 -0400

    Use tileset.c to support tiles on SDL2.
    This removes the dependency on SDL2_image, and gets PNG support working
    on Windows; SDL2_image crashes when trying to load a PNG on Windows.

--- win/share/tileset.c
+++ win/share/tileset.c
@@ -1,6 +1,17 @@
 #include "hack.h"
+#ifdef WIN32
+/* Need Windows APIs to load the default bitmap */
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include "winMS.h"
+#include "resource.h"
+#endif
 #include "tileset.h"
 
+#ifdef WIN32
+static boolean FDECL(load_builtin_image, (struct TileSetImage *));
+#endif
+static void FDECL(define_palette, (struct TileSetImage *));
 static void FDECL(get_tile_map, (const char *));
 static void FDECL(split_tiles, (const struct TileSetImage *));
 
@@ -63,27 +74,41 @@ boolean true_color;
     image->tile_width = 0;
     image->tile_height = 0;
 
-    /* Identify the image type */
-    fp = fopen(filename, "rb");
-    if (fp == NULL) goto error;
-    memset(header, 0, sizeof(header));
-    fread(header, 1, sizeof(header), fp);
-    fclose(fp);
-
-    /* Call the loader appropriate for the image */
-    if (memcmp(header, "BM", 2) == 0) {
-        ok = read_bmp_tiles(filename, image);
-    } else if (memcmp(header, "GIF87a", 6) == 0
-           ||  memcmp(header, "GIF89a", 6) == 0) {
-        ok = read_gif_tiles(filename, image);
-    } else if (memcmp(header, png_sig, sizeof(png_sig)) == 0) {
-        ok = read_png_tiles(filename, image);
-    } else if (memcmp(header, "/* XPM */", 9) == 0) {
-        ok = read_xpm_tiles(filename, image);
-    } else {
-        ok = FALSE;
+    if (filename == NULL || filename[0] == '\0') {
+#ifdef WIN32
+        /* On Win32, if no tilemap given, load the resource from
+         * the executable */
+        load_builtin_image(image);
+#endif
+        filename = "nhtiles.bmp";
     }
-    if (!ok) goto error;
+
+    if (image->pixels == NULL) {
+        /* Identify the image type */
+        fp = fopen(filename, "rb");
+        if (fp == NULL) goto error;
+        memset(header, 0, sizeof(header));
+        fread(header, 1, sizeof(header), fp);
+        fclose(fp);
+
+        /* Call the loader appropriate for the image */
+        if (memcmp(header, "BM", 2) == 0) {
+            ok = read_bmp_tiles(filename, image);
+        } else if (memcmp(header, "GIF87a", 6) == 0
+               ||  memcmp(header, "GIF89a", 6) == 0) {
+            ok = read_gif_tiles(filename, image);
+        } else if (memcmp(header, png_sig, sizeof(png_sig)) == 0) {
+            ok = read_png_tiles(filename, image);
+        } else if (memcmp(header, "/* XPM */", 9) == 0) {
+            ok = read_xpm_tiles(filename, image);
+        } else {
+            ok = FALSE;
+        }
+        if (!ok) goto error;
+    }
+
+    /* Provide a palette, if possible */
+    define_palette(image);
 
     /* Reject if the interface cannot handle direct color and the image does
        not use a palette */
@@ -105,6 +130,125 @@ boolean true_color;
 error:
     free_tile_image(image);
     return FALSE;
+}
+
+#ifdef WIN32
+static boolean
+load_builtin_image(image)
+struct TileSetImage *image;
+{
+    HDC memory_dc = NULL; /* custodial */
+    HBITMAP hbitmap = NULL; /* custodial */
+    BITMAP bm;
+    unsigned x, y;
+    size_t i;
+
+    /* We'll render to this device context */
+    memory_dc = CreateCompatibleDC(NULL);
+
+    /* Load the bitmap */
+    hbitmap = (HBITMAP) LoadImage(GetNHApp()->hApp,
+            MAKEINTRESOURCE(IDB_TILES), IMAGE_BITMAP,
+            0, 0, LR_DEFAULTSIZE);
+    if (hbitmap == NULL) goto error;
+
+    /* Render in the device context */
+    if (!SelectObject(memory_dc, (HGDIOBJ)hbitmap)) goto error;
+
+    /* Get the size of the bitmap */
+    GetObject(hbitmap, sizeof(BITMAP), &bm);
+
+    /* Populate the image structure */
+    image->width = bm.bmWidth;
+    image->height = bm.bmHeight;
+    image->pixels = (struct Pixel *) alloc(bm.bmWidth * bm.bmHeight
+                                           * sizeof(struct Pixel));
+
+    i = 0;
+    for (y = 0; y < bm.bmHeight; ++y) {
+        for (x = 0; x < bm.bmWidth; ++x) {
+            COLORREF color = GetPixel(memory_dc, x, y);
+            image->pixels[i].r = GetRValue(color);
+            image->pixels[i].g = GetGValue(color);
+            image->pixels[i].b = GetBValue(color);
+            image->pixels[i].a = 255;
+            ++i;
+        }
+    }
+
+    DeleteDC(memory_dc);
+    DeleteObject(hbitmap);
+    return TRUE;
+
+error:
+    DeleteDC(memory_dc);
+    DeleteObject(hbitmap);
+    free_tile_image(image);
+    return FALSE;
+}
+#endif
+
+static void
+define_palette(image)
+struct TileSetImage *image;
+{
+    unsigned x, y, ncolors, color;
+    size_t i;
+
+    if (image->indexes != NULL) return; /* already has a palette */
+
+    image->indexes = (unsigned char *) alloc(image->width * image->height);
+
+    ncolors = 0;
+    i = 0;
+    for (y = 0; y < image->height; ++y) {
+        for (x = 0; x < image->width; ++x) {
+            struct Pixel p1, p2;
+
+            /* Find the color in the palette as it exists */
+            p1 = image->pixels[i];
+            for (color = 0; color < ncolors; ++color) {
+                p2 = image->palette[color];
+                if (p1.r == p2.r && p1.g == p2.g
+                &&  p1.b == p2.b && p1.a == p2.a) {
+                    break;
+                }
+            }
+            if (color >= 256) {
+                /* More than 256 unique colors */
+                free(image->indexes);
+                image->indexes = NULL;
+                return;
+            }
+            if (color >= ncolors) {
+                /* New color */
+                image->palette[ncolors++] = p1;
+            }
+            image->indexes[i++] = color;
+        }
+    }
+}
+
+static int
+color_compare(c1, c2)
+const genericptr_t c1, c2;
+{
+    const struct Pixel *p1 = (const struct Pixel *) c1;
+    const struct Pixel *p2 = (const struct Pixel *) c2;
+    unsigned long i1, i2;
+
+    i1 = ((unsigned long) p1->r << 24)
+       | ((unsigned long) p1->g << 16)
+       | ((unsigned long) p1->b <<  8)
+       | ((unsigned long) p1->a <<  0);
+    i2 = ((unsigned long) p2->r << 24)
+       | ((unsigned long) p2->g << 16)
+       | ((unsigned long) p2->b <<  8)
+       | ((unsigned long) p2->a <<  0);
+
+    if (i1 < i2) return -1;
+    if (i1 > i2) return  1;
+    return 0;
 }
 
 const struct Pixel *
